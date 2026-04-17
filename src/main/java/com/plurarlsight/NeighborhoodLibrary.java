82cbaef5209b995062512aa878790c4ba9ee2f95
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-17 15:00:58 -0400

    Made a new class Book (made constructors, getters and setters). I started making an array for the books too.

--- src/main/java/com/plurarlsight/NeighborhoodLibrary.java
+++ src/main/java/com/plurarlsight/NeighborhoodLibrary.java
@@ -1,4 +1,10 @@
 package com.plurarlsight;
 
 public class NeighborhoodLibrary {
+    private static Book[] books = new Book[20];
+    private static int numBooks = 5;
+    public static void main(String[] args) {
+
+
+    }
 }
